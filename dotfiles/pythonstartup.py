2f9288cdd1b681da2df4aa1202438ec78b21e43e
elaforge <elaforge@google.com>
2009-06-17 18:01:54 +0000

    python3 doesn't have __builtin__

--- dotfiles/pythonstartup.py
+++ dotfiles/pythonstartup.py
@@ -3,21 +3,20 @@ import sys, os
 
 sys.ps1 = sys.ps2 = ''
 def _eh(typ, val, tb):
-	import local.qtraceback
-	s = ''.join(local.qtraceback.format_exception(typ, val, tb))
-	for line in s.split('\n'):
-		if line.strip():
-			sys.stdout.write('# %s\n' % line)
+    import local.qtraceback
+    s = ''.join(local.qtraceback.format_exception(typ, val, tb))
+    for line in s.split('\n'):
+        if line.strip():
+            sys.stdout.write('# %s\n' % line)
 sys.excepthook = _eh
 del _eh
 
-import __builtin__
 def _dh(object):
-	if object is None:
-		return
-	sys.stdout.write('#-> ')
-	sys.stdout.write(repr(object))
-	sys.stdout.write('\n')
-	__builtin__._ = object
+    if object is None:
+        return
+    sys.stdout.write('#-> ')
+    sys.stdout.write(repr(object))
+    sys.stdout.write('\n')
+    __builtins__._ = object
 sys.displayhook = _dh
 del _dh
